58c4ebe02516f520ad755a305faadb7057273e2a
Deyclan <brandon.piard@etu.univ-lyon1.fr>
2018-02-13 10:04:10 +0100

    Enum Event

---
+++ src/EventPOP3.java
@@ -0,0 +1,19 @@
+public enum EventPOP3 {
+
+    APOP("APOP"),
+    STAT("STAT"),
+    LIST("LIST"),
+    RETR("RETR"),
+    DELE("DELE"),
+    QUIT("QUIT");
+
+    private String cmd = "";
+
+    EventPOP3(String cmd){
+        this.cmd = cmd;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+}

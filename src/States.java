ae538a9bab15166fef27058dd6afbcede908c654
Deyclan <brandon.piard@etu.univ-lyon1.fr>
2018-02-13 09:52:11 +0100

    Enumeration etats APOP

---
+++ src/States.java
@@ -0,0 +1,16 @@
+public enum States {
+
+    AUTHORIZATION("AUTHORIZATION"),
+    TRANSACTION("TRANSACTION");
+
+    private String name = "";
+
+    States(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+}

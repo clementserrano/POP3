a8634289c442739fcb68c7a6bf9c0ad1774b9320
Clément Serrano <clementserrano3@gmail.com>
2018-03-04 20:08:09 +0100

    CS - Implémentation Gson et fichier database.json pour les données

--- src/Server/Connexion.java
+++ src/Server/Connexion.java
@@ -3,13 +3,17 @@ package Server;
 import Helpers.ConsoleColor;
 import Helpers.EventPOP3;
 import Helpers.States;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.net.Socket;
 import java.security.MessageDigest;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class Connexion implements Runnable {
 
@@ -22,7 +26,7 @@ public class Connexion implements Runnable {
     private static Map<String, Map<Integer, String>> boitesMail;
 
 
-    public Connexion(Socket socket){
+    public Connexion(Socket socket) {
         this.socket = socket;
     }
 
@@ -42,8 +46,8 @@ public class Connexion implements Runnable {
             String user = "";
             Map<Integer, String> boiteMail = new HashMap<>();
 
-            while (socket.isConnected()){
-                if ((message = in.readLine()) != null ) {
+            while (socket.isConnected()) {
+                if ((message = in.readLine()) != null) {
                     print(message);
 
                     String[] array = message.split(" ");
@@ -55,11 +59,11 @@ public class Connexion implements Runnable {
                             switch (etat) {
                                 case AUTHORIZATION:
                                     // Vérifie si user existe
-                                    if(users.keySet().contains(param)){
+                                    if (users.keySet().contains(param)) {
                                         // Vérifie si pass OK
                                         String pass = array[2];
-                                        if(MessageDigest.getInstance("MD5").digest(users.get(param).getBytes())
-                                                .equals(MessageDigest.getInstance("MD5").digest(pass.getBytes()))){
+                                        if (MessageDigest.getInstance("MD5").digest(users.get(param).getBytes())
+                                                .equals(MessageDigest.getInstance("MD5").digest(pass.getBytes()))) {
                                             user = param;
                                             boiteMail = boitesMail.get(user);
                                             out.write("+OK " + user + " a " + boiteMail.size() + " messages.");
@@ -67,14 +71,14 @@ public class Connexion implements Runnable {
                                     }
                                     break;
                                 case TRANSACTION:
-                                    out.write("Commande "+ message +" ignorée");
+                                    out.write("Commande " + message + " ignorée");
                                     break;
                             }
                             break;
                         case STAT:
                             switch (etat) {
                                 case AUTHORIZATION:
-                                    out.write("Commande "+ message +" ignorée");
+                                    out.write("Commande " + message + " ignorée");
                                     break;
                                 case TRANSACTION:
                                     out.write("+OK nbMails : " + boiteMail.size() + " taille : " + nbOctet(boiteMail)
@@ -85,18 +89,18 @@ public class Connexion implements Runnable {
                         case RETR:
                             switch (etat) {
                                 case AUTHORIZATION:
-                                    out.write("Commande "+ message +" ignorée");
+                                    out.write("Commande " + message + " ignorée");
                                     break;
                                 case TRANSACTION:
                                     // Vérifie si le numéro correspond
                                     Integer nMail = Integer.valueOf(param);
-                                    if(boiteMail.keySet().contains(nMail)){
+                                    if (boiteMail.keySet().contains(nMail)) {
                                         // Construit la réponse
                                         String mail = boiteMail.get(nMail);
                                         String mes = "+OK " + mail.getBytes().length + " octets\n";
                                         mes += mail;
                                         out.write(mes);
-                                    }else{
+                                    } else {
                                         out.write("-ERR");
                                     }
                                     break;
@@ -122,37 +126,40 @@ public class Connexion implements Runnable {
             System.out.println("Server.Server.Connexion closed");
             socket.close();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("An error occurred");
             e.printStackTrace();
         }
     }
 
-    private int nbOctet(Map<Integer,String> mails) {
+    private int nbOctet(Map<Integer, String> mails) {
         int res = 0;
-        for(Map.Entry<Integer,String> m : mails.entrySet()){
+        for (Map.Entry<Integer, String> m : mails.entrySet()) {
             res += m.getValue().getBytes().length;
         }
         return res;
     }
 
     private void initData() {
-        // Users
-        users = new HashMap<>();
-        users.put("Jean","Karim");
+        Gson gson = new Gson();
+        try {
+            FileReader file = new FileReader("src/Server/database.json");
+            JsonObject json = gson.fromJson(file, JsonObject.class);
+            // Users
+            Type usersType = new TypeToken<Map<String, String>>() {}.getType();
+            users = gson.fromJson(json.get("users").getAsJsonObject(), usersType);
 
-        // Boites mail
-        boitesMail = new HashMap<>();
-        Map<Integer, String> mails = new HashMap<>();
-        mails.put(1,"Jérome ta mère");
-        mails.put(2,"Philippe ton père");
-        boitesMail.put("Jean", mails);
+            // Boites mail
+            Type boitesMailType = new TypeToken<Map<String, Map<Integer, String>>>() {}.getType();
+            boitesMail = gson.fromJson(json.get("boitesMail").getAsJsonObject(), boitesMailType);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void print(String message){
+    private void print(String message) {
         System.out.print(ConsoleColor.ANSI_CYAN);
         System.out.print(socket.getInetAddress() + " said : " + ConsoleColor.ANSI_RESET);
         System.out.println(message);
     }
-
 }

efb2ab65d8bd8da0b3da23384b5604da013147a6
Deyclan <brandon.piard@etu.univ-lyon1.fr>
2018-03-04 14:31:43 +0100

    Première GUI

--- src/Client/ClientLauncher.java
+++ src/Client/ClientLauncher.java
@@ -1,28 +1,18 @@
 package Client;
 
-import Client.GUI.Controllers.ConnexionController;
 import Helpers.Constants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ClientLauncher extends Application {
 
     static AnchorPane root;
-    static List<Pane> panes = new ArrayList<>();
-    static int currentPane = 0;
-
     private FXMLLoader fxmlLoader;
-    private final URL connexionStageURL = getClass().getResource("/Client/GUI/Views/connexionStage.fxml");
-    private final URL mainStageURL = getClass().getResource("/Client/GUI/Views/mainStage.fxml");
+    private final URL frameURL = getClass().getResource("/Client/GUI/Test/frame.fxml");
 
     public static void main(String[] args) {
         Application.launch(ClientLauncher.class, args);
@@ -30,37 +20,10 @@ public class ClientLauncher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        fxmlLoader = new FXMLLoader(mainStageURL);
+        fxmlLoader = new FXMLLoader(frameURL);
         root = fxmlLoader.load();
-
-        panes.add(FXMLLoader.load(connexionStageURL));
-
-
         primaryStage.setScene(new Scene(root, Constants.frameDimension.width,Constants.frameDimension.height));
         primaryStage.show();
     }
 
-    public static Pane getPane(stage stage){
-        return panes.get(stage.getId());
-    }
-
-    public static void setPane(stage stage){
-        root.getChildren().remove(panes.get(currentPane));
-        root.getChildren().add(getPane(stage));
-        currentPane = stage.getId();
-    }
-
-    public enum stage {
-        //MAIN(0),
-        CONNEXION(0);
-
-        private int id;
-        stage(int id){
-            this.id = id;
-        }
-
-        public int getId() {
-            return id;
-        }
-    }
 }
